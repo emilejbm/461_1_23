865852be04f6096b141077c144b00faed9b7942d
Emile Baez <ebaezmunne@gmail.com>
2023-01-24 16:48:23 -0500

    ini commit for cli folder structure

---
+++ commands/CLI/root.go
@@ -0,0 +1,23 @@
+package cli
+
+import (
+	"fmt"
+	"os"
+	"github.com/spf13/cobra"
+)
+
+var rootCmd = &cobra.Command{
+	Use: "team23",
+	Short: "team23 - root command for app"
+	Long: "team23 is the root command to navigate through Team 23's CLI app",
+	Run: func(cmd *cobra.Command, args[]string){
+
+	},
+}
+
+func Execute() {
+	if err := rootCmd.Execute(); err != nil {
+		fmt.Fprintf(os.Stderr, "Error using CLI '%s'", err)
+		os.Exit(1)
+	}
+}
