92d4c760aa1f75e7468e88861494ff3511beb612
Mimi Chon <chonm@purdue.edu>
2023-01-29 20:16:56 -0500

    fixed formatting

--- run.sh
+++ run.sh
@@ -3,11 +3,11 @@
 #include check for num of arguments
 case $1 in
     "install")
-        echo -n "will install dependencies tbd";; 
+        echo "will install dependencies tbd";; 
     "build")
-        echo -n "will compile tbd";;
+        echo "will compile tbd";;
     "URL_FILE") # i have no idea how to actually do this maybe regex ?
-        echo -n "will process URLs tbd";;
+        echo "will process URLs tbd";;
     "test") # logging and testing stuff
-        echo -n "will test and log tbd";;
+        echo "will test and log tbd";;
 esac
